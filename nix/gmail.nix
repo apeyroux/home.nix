900314755d13a14d424999d843a3f979af6702dc
Alexandre Peyroux <alex@px.io>
2020-05-15 00:05:54 +0200

    snap

--- nix/gmail.nix
+++ nix/gmail.nix
@@ -7,31 +7,31 @@ with import <nixpkgs> {};
 
   services.imapnotify.enable = true;
 
-  accounts.email.accounts.gmail = {
-    primary = false;
-    address = "peyroux@gmail.com";
-    flavor = "gmail.com";
-    passwordCommand = "${gnupg}/bin/gpg2 -q --for-your-eyes-only --no-tty -d ~/.mailpass-gmail.gpg";
-    imapnotify = {
-      enable = true;
-      boxes = ["Inbox" "Famille"];
-      onNotifyPost = {
-        mail = "${libnotify}/bin/notify-send 'Nouveau mail!'";
-      };
-      onNotify = "${isync}/bin/mbsync -a";
-    };
-    msmtp = {
-      enable = true;
-    };
-    mbsync = {
-      enable = true;
-      create = "both";
-      patterns = ["*" "![Gmail]/Spam"];
-      extraConfig.remote = {
-        UseNamespace = "no";
-      };
-    };
-  }; 
+  # accounts.email.accounts.gmail = {
+  #   primary = false;
+  #   address = "peyroux@gmail.com";
+  #   flavor = "gmail.com";
+  #   passwordCommand = "${gnupg}/bin/gpg2 -q --for-your-eyes-only --no-tty -d ~/.mailpass-gmail.gpg";
+  #   imapnotify = {
+  #     enable = true;
+  #     boxes = ["Inbox" "Famille"];
+  #     onNotifyPost = {
+  #       mail = "${libnotify}/bin/notify-send 'Nouveau mail!'";
+  #     };
+  #     onNotify = "${isync}/bin/mbsync -a";
+  #   };
+  #   msmtp = {
+  #     enable = true;
+  #   };
+  #   mbsync = {
+  #     enable = true;
+  #     create = "both";
+  #     patterns = ["*" "![Gmail]/Spam"];
+  #     extraConfig.remote = {
+  #       UseNamespace = "no";
+  #     };
+  #   };
+  # }; 
 
   accounts.email.accounts.px = {
     primary = true;
